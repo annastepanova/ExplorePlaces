21972ae726ed99ebd976c705b8b4f6730d8b709f
annastepanova <annastepanova1811@gmail.com>
2020-04-13 21:31:15 -0400

    added Avatar and Card styling components

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,21 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Avatar from '../../shared/components/UIElements/Avatar'
+import Card from '../../shared/components/UIElements/Card'
 import './UserItem.css'
 
 const UserItem = (props) => {
 
   return (
     <li className="user-item">
-      <div className="user-item_content">
-        <div className="user-item_image">
-          <img src={props.image} alt={props.name}/>
-        </div>
-        <div className="user-item_info">
-          <h2>{props.name}</h2>
-          <h3>
-            {props.placecount} {props.placecount === 1 ? 'Place' : 'Places'}
-          </h3>
-        </div>
-      </div>
+        <Card className="user-item_content">
+          <Link to={`/${props.id}/places`}>
+            <div className="user-item_image">
+              <Avatar image={props.image} alt={props.name}/>
+            </div>
+            <div className="user-item_info">
+              <h2>{props.name}</h2>
+              <h3>
+                {props.placecount} {props.placecount === 1 ? 'Place' : 'Places'}
+              </h3>
+            </div>
+          </Link>
+        </Card>
     </li>
   )
 
