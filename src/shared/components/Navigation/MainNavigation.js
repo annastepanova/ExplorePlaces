d6bb795a7c95cc84e5d7516659e97dc166881657
annastepanova <annastepanova1811@gmail.com>
2020-04-15 11:09:46 -0400

    handling SideBar open-close state

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import MainHeader from './MainHeader'
 import NavLinks from '../Navigation/NavLinks'
 import SideBar from '../Navigation/SideBar'
+import Backdrop from '../UIElements/Backdrop'
 import './MainNavigation.css'
 
 
 const MainNavigation = () => {
+  const [sideBarIsOpen, setBarIsOpen] = useState(false)
+
+  const openSideBar = () => {
+    setBarIsOpen(true)
+  }
+
+  const closeSideBar = () => {
+    setBarIsOpen(false)
+  }
 
   return (
     <>
+    {sideBarIsOpen && <Backdrop onClick={closeSideBar}/>}
+    {
+      sideBarIsOpen ? (
     <SideBar>
       <nav className="main-navigation__drawer-nav">
         <NavLinks/>
       </nav>
-    </SideBar>
+    </SideBar> ) : null
+    }
     <MainHeader>
-      <button className="main-navigation__menu-btn">
+      <button className="main-navigation__menu-btn" onClick={openSideBar}>
         <span />
         <span />
         <span />
