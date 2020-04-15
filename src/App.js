e49200f421d26ae4f5440e95e0874c42b09c2d94
annastepanova <annastepanova1811@gmail.com>
2020-04-14 20:05:01 -0400

    added MainHeader

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 import Users from './user/pages/Users'
 import NewPlace from './places/pages/NewPlace'
+import MainNavigation from './shared/components/Navigation/MainNavigation'
 
 const App = () => {
 
   return (
     <Router>
-      <Switch>
-      <Route exact path="/" component={Users}/>
-      <Route exact path="/places/new" component={NewPlace}/>
-      <Redirect to="/"/>
-      </Switch>
+      <MainNavigation/>
+        <main>
+          <Switch>
+            <Route exact path="/" component={Users}/>
+            <Route exact path="/places/new" component={NewPlace}/>
+            <Redirect to="/"/>
+          </Switch>
+        </main>
     </Router>
   )
 
