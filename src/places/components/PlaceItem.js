c7669fa09b28fedc2cfc49bcbfb357b9fadb4baa
annastepanova <annastepanova1811@gmail.com>
2020-05-24 12:00:27 -0400

    fixing minor bugs

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -82,10 +82,10 @@ const PlaceItem = (props) => {
         </div>
         <div className="place-item__actions">
           <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
-          {auth.isLoggedIn && (
+          {auth.userId === props.creatorId && (
             <Button to={`/places/${props.id}`}>EDIT</Button>
           )}
-          {auth.isLoggedIn && (
+            {auth.userId === props.creatorId && (
             <Button danger onClick={showDeleteWarning}>DELETE</Button>
           )}
         </div>
