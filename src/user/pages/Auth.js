399c5a3a57c61d1efb0c6ef80757ef1229791494
annastepanova <annastepanova1811@gmail.com>
2020-05-26 20:41:03 -0400

    sending POST req with attached image file to the backend

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -58,7 +58,6 @@ const Auth = () => {
   }
 
 
-
   const authFormSubmit = async (event) => {
     event.preventDefault()
     console.log(formState.inputs)
@@ -81,16 +80,15 @@ const Auth = () => {
     }
     else {
       try {
+        const formData = new FormData()
+        formData.append('email', formState.inputs.email.value)
+        formData.append('name', formState.inputs.name.value)
+        formData.append('password', formState.inputs.password.value)
+        formData.append('image', formState.inputs.image.value)
+
         const responseData = await sendRequest('http://localhost:5000/api/users/signup',
           'POST',
-          JSON.stringify({
-            name: formState.inputs.name.value,
-            email: formState.inputs.email.value,
-            password: formState.inputs.password.value
-          }),
-          {
-            'Content-Type': 'application/json'
-          }
+          formData
         )
         auth.login(responseData.user.id)
       }
