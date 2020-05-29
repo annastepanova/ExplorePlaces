6d1f9333bb77baf9e36ba81ba1cedfbec63754ae
annastepanova <annastepanova1811@gmail.com>
2020-05-29 18:02:34 -0400

    auto-login and auto-logout feature

--- src/App.js
+++ src/App.js
@@ -8,15 +8,19 @@ import UpdatePlace from './places/pages/UpdatePlace'
 import Auth from './user/pages/Auth'
 import { AuthContext } from './shared/context/AuthContext'
 
+let logoutTimer 
+
 const App = () => {
 
   const [token, setToken] = useState(false)
+  const [tokenExpirationDate, setTokenExpirationDate] = useState()
   const [userId, setUserId] = useState(false)
 
   const login = useCallback((uid, token, expirationDate) => {
     setToken(token)
     setUserId(uid)
     const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60)
+    setTokenExpirationDate(tokenExpirationDate)
     localStorage.setItem('userData', JSON.stringify({
       userId: uid, 
       token: token,
@@ -26,9 +30,20 @@ const App = () => {
 
   const logout = useCallback(() => {
     setToken(null)
+    setTokenExpirationDate(null)
     setUserId(null)
     localStorage.removeItem('userData')
   }, [])
+
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime()
+      logoutTimer = setTimeout(logout, remainingTime)
+    }
+    else {
+      clearTimeout(logoutTimer)
+    }
+  }, [token, logout, tokenExpirationDate])
 
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem('userData'))
